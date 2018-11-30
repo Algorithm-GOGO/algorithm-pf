988844bb34f581c91f2e2f9c51fd40f4b955f0a5
roharon <roharon98@gmail.com>
2018-11-30 21:18:48 +0900

    content, user 추가

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/keyboard')
+def Keyboard(request):
+
+    json_str = (request.body).decode('utf-8')
+    received_json = json.loads(json_str)
+
+    content_name = received_json['content']
+    # 누른 버튼 이름
+    user_name = received_json['user_key']
+    # 유저네임
 
 
 if __name__ == '__main__':
